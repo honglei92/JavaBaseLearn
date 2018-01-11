1ef90c6c649cd4aa3c84b159a6257292bdf602df
wanghonglei <wanghonglei@boco.com.cn>
2018-01-11 12:59:00 +0800

    集合相关

---
+++ src/LightTest.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class LightTest {
+	public static void main(String[] args) {
+		int a = 200;
+		int b = 10;
+		System.out.println(new Date().toGMTString());
+		for (int i = 0; i < a; i++) {
+			boolean current = false;
+			if (i % 2 == 0) {
+				current = true;
+			}
+			for (int j = 3; j < b + 1; j++) {
+				if (i % j == 0) {
+					current = !current;
+				}
+			}
+			if (current == true) {
+				System.out.print(i + ",");
+			}
+		}
+		System.out.println("\n" + new Date().toGMTString());
+	}
+}
