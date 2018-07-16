d710f5dc962d04c013b1dbbe5967db3cea8deff9
1667849141@qq.com <wanghonglei1>
2018-07-16 23:51:29 +0800

    集合接口、类和Arrays，Collections操作类
    Signed-off-by: 1667849141@qq.com <wanghonglei1>

--- src/builtinlib/map/ConcurrentHashMapTest.java
+++ src/builtinlib/map/ConcurrentHashMapTest.java
@@ -2,6 +2,7 @@ package builtinlib.map;
 
 import util.L;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,6 +13,38 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConcurrentHashMapTest {
     public static void main(String[] args) {
         test();
+//        testHashMap();
+    }
+
+    private static void testHashMap() {
+        Map<String, String> mHashMap = new HashMap<>();
+        mHashMap.put("name", "whl");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    if (i == 1) {
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mHashMap.put("name", "honglei92" + i);
+                    L.p(i + mHashMap.get("name"));
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    mHashMap.put("name", "wanghonglei" + i);
+                    L.p(i + mHashMap.get("name"));
+                }
+            }
+        }).start();
+
     }
 
     private static void test() {
@@ -21,8 +54,15 @@ public class ConcurrentHashMapTest {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    mMap.put("name", "honglei92");
-                    L.p(mMap.get("name"));
+                    if (i == 1) {
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mMap.put("name", "honglei92" + i);
+                    L.p("a" + mMap.get("name"));
                 }
             }
         }).start();
@@ -30,8 +70,8 @@ public class ConcurrentHashMapTest {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    mMap.put("name", "wanghonglei");
-                    L.p(mMap.get("name"));
+                    mMap.put("name", "wanghonglei" + i);
+                    L.p("b" + mMap.get("name"));
                 }
             }
         }).start();
--- src/builtinlib/map/HashMapTest.java
+++ src/builtinlib/map/HashMapTest.java
@@ -1,6 +1,7 @@
 package builtinlib.map;
 
 
+import util.L;
 import util.S;
 
 import java.util.HashMap;
@@ -13,9 +14,36 @@ import java.util.Map;
  */
 public class HashMapTest {
     public static void main(String[] args) {
+        mHashCode();
+//        test();
+    }
+
+    /**
+     * hashcode测试
+     */
+    private static void mHashCode() {
+        String a = "123";
+        String b = "123";
+        Integer c = 123;
+        Object o = new Object();
+        L.p(a.hashCode() + "");
+        L.p(b.hashCode() + "");
+        L.p(o.hashCode() + "");
+        L.p(o.toString());
+        L.p(Integer.toHexString(o.hashCode()));
+        L.p(c.hashCode() + "");
+        Computer mComputer = new Computer();
+        L.p((mComputer.hashCode() + ""));
+        L.p((new Computer().hashCode() + ""));
+        L.p((new Computer().hashCode() + ""));
+        L.p((new Computer().hashCode() + ""));
+    }
+
+    private static void test() {
+//            Map<String, String> map = new HashMap<>(2 ^ 4, 0.75f);
+        Map<String, String> map = new HashMap<>(16, 0.75f);
         S.showNow();
         for (int i = 0; i < 100000; i++) {
-            Map<String, String> map = new HashMap<>(2 ^ 1, 1);
             map.put(i + "", "王宏雷");
         }
         S.showNow();
