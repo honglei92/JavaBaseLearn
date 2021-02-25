91cb461c7ac551fcd35648e7ca9d61710a27be9b
王宏雷 <wanghonglei@360tianma.com>
2021-02-25 20:02:57 +0800

    优化软著代码

--- src/javawhl/file/RuanZhu.java
+++ src/javawhl/file/RuanZhu.java
@@ -23,6 +23,7 @@ public class RuanZhu {
 
     public static void main(String[] args) {
         System.out.println("arg0");
+        // folderMethod2("D:\\01whl\\07工作文档\\软著申请\\软著代码安卓");
         folderMethod2("D:\\01whl\\07工作文档\\软著申请\\软著代码ios");
         for (int i = 0; i < 20; i++) {
             buildFile(i);
@@ -56,7 +57,7 @@ public class RuanZhu {
         // 不关闭文件会导致资源的泄露，读写文件都同理
         // Java7的try-with-resources可以优雅关闭文件，异常时自动关闭文件；详细解读https://stackoverflow.com/a/12665271
         try (InputStreamReader reader = new InputStreamReader(new FileInputStream(pathname), "UTF-8");
-         BufferedReader br = new BufferedReader(reader) // 建立一个对象，它把文件内容转成计算机能读懂的语言
+                BufferedReader br = new BufferedReader(reader) // 建立一个对象，它把文件内容转成计算机能读懂的语言
         ) {
             String line;
             // 网友推荐更加简洁的写法
@@ -64,8 +65,20 @@ public class RuanZhu {
             while ((line = br.readLine()) != null) {
                 // 一次读入一行数据
                 System.out.println(line);
+                if (line.trim().startsWith("private")) {
+                    oneFile += "//" + Word.worlds[(int) (Math.random() * 115)] + "\r\n";
+                }
+                if (line.trim().startsWith("@Override")) {
+                    oneFile += "//" + Word.worlds[(int) (Math.random() * 115)] + "\r\n";
+                }
+                if (line.trim().startsWith("package")) {
+                    oneFile += "//声明你的包名\r\n";
+                }
                 lines++;
                 oneFile += (line + "\r\n");
+                if (line.trim().endsWith("{")) {
+                    oneFile += "//" + Word.worlds[(int) (Math.random() * 115)] + "\r\n";
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -74,13 +87,14 @@ public class RuanZhu {
 
     private static void writeFile(int i, String line) {
         try {
+            // File writeName = new File("D:\\01whl\\07工作文档\\软著申请\\软著输出安卓\\" + i + "output_android.doc"); // 相对路径，如果没有则要建立一个新的output.txt文件
             File writeName = new File("D:\\01whl\\07工作文档\\软著申请\\软著输出ios\\" + i + "output_ios.doc"); // 相对路径，如果没有则要建立一个新的output.txt文件
             if (!writeName.exists())
                 writeName.createNewFile(); // 创建新文件,有同名的文件的话直接覆盖
             try (
-                // FileWriter writer = new FileWriter(writeName); 
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(writeName), "UTF-8");
-            BufferedWriter out = new BufferedWriter(writer)) {
+                    // FileWriter writer = new FileWriter(writeName);
+                    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(writeName), "UTF-8");
+                    BufferedWriter out = new BufferedWriter(writer)) {
                 out.write(line + "\r\n"); // \r\n即为换行
                 // out.flush(); // 把缓存区内容压入文件
             }
