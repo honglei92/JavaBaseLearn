9f46817a117460d0a0ac1ce133d4bc2424ff23a8
王宏雷 <wanghonglei@360tianma.com>
2021-03-28 14:45:13 +0800

    LeetCode20

---
+++ src/datastructure/leetcode/LeetCode20.java
@@ -0,0 +1,49 @@
+package datastructure.leetcode;
+
+import java.util.Stack;
+import java.util.Vector;
+
+import jdk.internal.jshell.tool.resources.version;
+
+public class LeetCode20 {
+    public static void main(String[] args) {
+        System.out.println(isValid("{{}{}[]"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '(') {// 左括弧进栈
+                stack.push(s.charAt(i));
+            }
+            if (s.charAt(i) == '}' || s.charAt(i) == ']' || s.charAt(i) == ')') {// 右括弧进栈
+                if (stack.empty()) {
+                    return false;
+                } else {
+                    if (match(s.charAt(i), stack.lastElement())) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    private static boolean match(char charAt, Character firstElement) {
+        if (charAt == ')' && firstElement.charValue() == '(') {
+            return true;
+        }
+        if (charAt == '}' && firstElement.charValue() == '{') {
+            return true;
+        }
+        if (charAt == ']' && firstElement.charValue() == '[') {
+            return true;
+        }
+        return false;
+    }
+}
